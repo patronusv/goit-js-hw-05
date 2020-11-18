1b497773710b1973a4b16b53cd696457b3e7fb42
Viktor Gorokhov <patronusv@gmail.com>
2020-11-18 18:53:29 +0200

    добавляет решение 4-го задания

--- js/task-04.js
+++ js/task-04.js
@@ -1,1 +1,30 @@
 'use strict';
+class StringBuilder {
+  constructor(string) {
+    this._value = string;
+  }
+  get value() {
+    return this._value;
+  }
+  append(str) {
+    return (this._value = `${this._value}${str}`);
+  }
+  prepend(str) {
+    return (this._value = `${str}${this._value}`);
+  }
+  pad(str) {
+    this.prepend(str);
+    this.append(str);
+  }
+}
+
+const builder = new StringBuilder('.');
+
+builder.append('^');
+console.log(builder.value); // '.^'
+
+builder.prepend('^');
+console.log(builder.value); // '^.^'
+
+builder.pad('=');
+console.log(builder.value); // '=^.^='
